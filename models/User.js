80909d2da787155cab06114e8f6b17881a7a0962
Damon Kalaboukis <damonkala@yahoo.com>
2016-02-27 22:04:19 -0800

    Pick up much later

--- models/User.js
+++ models/User.js
@@ -1,64 +1,95 @@
 'use strict';
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt');
-var jwt = require('jwt-simple');
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+// var bcrypt = require('bcrypt');
+// var jwt = require('jwt-simple');
 
-var User;
+var stormpath = require('stormpath');
 
-var userSchema = Schema({
-	email:{type: String, required: true, unique: true},
-	username: { type: String, required: true, unique: true},
-	name: {type: String, required: true},
-	password: { type: String, required: true },
-	location: { type: String},
-	interests: [{type: Schema.Types.ObjectId, ref: "Interest"}],
-	alerts: { type: Array}
+var apiKey = new stormpath.ApiKey(
+  process.env['STORMPATH_CLIENT_APIKEY_ID'],
+  process.env['STORMPATH_CLIENT_APIKEY_SECRET']
+);
+
+var client = new stormpath.Client({ apiKey: apiKey });
+
+var applicationHref = process.env['STORMPATH_APPLICATION_HREF'];
+
+client.getApplication(applicationHref, function(err, application) {
+  console.log('Application:', application);
+});
+
+var account = {
+  givenName: 'Joe',
+  surname: 'Stormtrooper',
+  username: 'tk421',
+  email: 'tk421@stormpath.com',
+  password: 'Changeme1',
+  customData: {
+    favoriteColor: 'white'
+  }
+};
+
+application.createAccount(account, function(err, createdAccount) {
+  console.log('Account:', createdAccount);
 });
 
 
-userSchema.statics.register = function(user, cb){
-	var username = user.username;
-	var email = user.email;
-	var name = user.name;
-	var city = user.city;
-	bcrypt.genSalt(10, function(err, salt) {
-		bcrypt.hash(user.password, salt, function(err, password) {
-			User.find({$or: [{username: username}, {email: email}] }, function(err, user){
-				if (err || user[0]){return console.log(err) || console.log("Username or email already exists")}
-				var newUser = new User;
-				newUser.username = username;
-				newUser.email = email;
-				newUser.name = name;
-				newUser.location = city;
-				newUser.password = password;
-				newUser.save(function(err, savedUser){
-					savedUser.password = null;
-					cb(err, savedUser)
-				})
-			})
-		});
-	});
-}
-
-
-userSchema.statics.login = function(user, cb){
-	var username = user.username;
-	var password = user.password;
-	User.findOne({username: username}).populate('interests').exec(function(err, dbUser){
-		console.log("Is our user okay?", dbUser);
-		if(err || !dbUser) return cb(err || 'Incorrect username or password');
-		bcrypt.compare(user.password, dbUser.password, function(err, correct){
-			if(err || !correct) return cb(err || 'Incorrect username or password');
-			dbUser.password = null;
-			dbUser.avatar = null
-			cb(null, dbUser);
-		})
-	})
-}
-
-User = mongoose.model('User', userSchema);
-
-
-module.exports = User;
+// var User;
+//
+// var userSchema = Schema({
+// 	email:{type: String, required: true, unique: true},
+// 	username: { type: String, required: true, unique: true},
+// 	name: {type: String, required: true},
+// 	password: { type: String, required: true },
+// 	location: { type: String},
+// 	interests: [{type: Schema.Types.ObjectId, ref: "Interest"}],
+// 	alerts: { type: Array}
+// });
+//
+//
+// userSchema.statics.register = function(user, cb){
+// 	var username = user.username;
+// 	var email = user.email;
+// 	var name = user.name;
+// 	var city = user.city;
+// 	bcrypt.genSalt(10, function(err, salt) {
+// 		bcrypt.hash(user.password, salt, function(err, password) {
+// 			User.find({$or: [{username: username}, {email: email}] }, function(err, user){
+// 				if (err || user[0]){return console.log(err) || console.log("Username or email already exists")}
+// 				var newUser = new User;
+// 				newUser.username = username;
+// 				newUser.email = email;
+// 				newUser.name = name;
+// 				newUser.location = city;
+// 				newUser.password = password;
+// 				newUser.save(function(err, savedUser){
+// 					savedUser.password = null;
+// 					cb(err, savedUser)
+// 				})
+// 			})
+// 		});
+// 	});
+// }
+//
+//
+// userSchema.statics.login = function(user, cb){
+// 	var username = user.username;
+// 	var password = user.password;
+// 	User.findOne({username: username}).populate('interests').exec(function(err, dbUser){
+// 		console.log("Is our user okay?", dbUser);
+// 		if(err || !dbUser) return cb(err || 'Incorrect username or password');
+// 		bcrypt.compare(user.password, dbUser.password, function(err, correct){
+// 			if(err || !correct) return cb(err || 'Incorrect username or password');
+// 			dbUser.password = null;
+// 			dbUser.avatar = null
+// 			cb(null, dbUser);
+// 		})
+// 	})
+// }
+//
+// User = mongoose.model('User', userSchema);
+//
+//
+// module.exports = User;
